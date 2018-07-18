8e7cfd4dbcf8462a8baec069c2dca4f68c85af9c
MarozasZ <Zilvinas.Marozas@moodys.com>
2018-07-18 17:27:00 -0400

    changes

--- udacity-mlnd/grains_futures_prices_prognosticator/visuals.py
+++ udacity-mlnd/grains_futures_prices_prognosticator/visuals.py
@@ -32,3 +32,32 @@ def plot_numerical_series(data):
 		plt.title(data.columns[group], y=0.5, loc='right')
 		i += 1
 	plt.show()
+
+def plot_lstm_prediction(actual, prediction, title='Google Trading vs Prediction', y_label='Price USD', x_label='Trading Days'):
+    """
+    Plots train, test and prediction
+    :param actual: DataFrame containing actual data
+    :param prediction: DataFrame containing predicted values
+    :param title:  Title of the plot
+    :param y_label: yLabel of the plot
+    :param x_label: xLabel of the plot
+    :return: prints a Pyplot againts items and their closing value
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # Add labels
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+
+    # Plot actual and predicted close values
+
+    plt.plot(actual, '#00FF00', label='Adjusted Close')
+    plt.plot(prediction, '#0000FF', label='Predicted Close')
+
+    # Set title
+    ax.set_title(title)
+    ax.legend(loc='upper left')
+
+
+    plt.show()
