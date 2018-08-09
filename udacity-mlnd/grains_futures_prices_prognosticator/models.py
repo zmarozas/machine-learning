976a466a9024c4dcb87b4918f4c193e515ab567a
MarozasZ <Zilvinas.Marozas@moodys.com>
2018-08-09 09:35:47 -0400

    changes

--- udacity-mlnd/grains_futures_prices_prognosticator/models.py
+++ udacity-mlnd/grains_futures_prices_prognosticator/models.py
@@ -1,10 +1,42 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from numpy import concatenate
+from math import sqrt
+from sklearn.metrics import mean_squared_error
 # persistence model
 def model_persistence(x):
     return x
-    
+
+
+def make_benchmark_model_prediction(bench_x,bench_y):
+    # walk-forward validation
+    predictions = list()
+    for x in bench_x:
+        yhat = model_persistence(x)
+        predictions.append(yhat)
+    rmse = sqrt(mean_squared_error(bench_y, predictions))
+    return predictions,rmse
+
+
+def make_lstm_prediction(x_data,y_data,model,scaler):
+    # make a prediction
+    yhat = model.predict(x_data)
+    x_data_eval = x_data.reshape((x_data.shape[0], x_data.shape[2]))
+    # invert scaling for forecast
+    inv_yhat = concatenate((yhat, x_data_eval[:, 1:]), axis=1)
+    inv_yhat = scaler.inverse_transform(inv_yhat)
+    inv_yhat = inv_yhat[:,0]
+    # invert scaling for actual
+    y_data_eval = y_data.reshape((len(y_data), 1))
+    inv_y = concatenate((y_data_eval, x_data_eval[:, 1:]), axis=1)
+    inv_y = scaler.inverse_transform(inv_y)
+    inv_y = inv_y[:,0]
+    # calculate RMSE
+    rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+    return inv_yhat, inv_y, rmse   
+
+
 def basic_lstm_model(train_X,train_y,validation_X,validation_y):
     model = Sequential()
     model.add(LSTM(1, input_shape=(train_X.shape[1], train_X.shape[2])))
